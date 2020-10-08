135418bafd59683fdd7dc42d399c97984b983ebd
benjaminhuanghuang <bhuang@rms.com>
2020-10-07 20:49:44 -0700

    get user and pass

---
+++ _notes/firebase.md
@@ -0,0 +1,17 @@
+## Create project
+Register App
+
+Copy the firebaseConfig into project
+
+
+## Auth
+
+
+
+## Database
+Cloud Firestore -> Create database
+
+
+
+
+ 
--- video-chat/src/App.vue
+++ video-chat/src/App.vue
@@ -1,20 +1,34 @@
 <template>
   <div id="app">
-    <Navigation />
-    <router-view />
+    <Navigation :user="user"/>
+    <router-view :user="user"/>
   </div>
 </template>
 
-
 <script>
+import db from './db.js'
 import Navigation from '@/components/Navigation'
+
 export default {
+  name:'App',
+  data: function() {
+    return {
+      user: null
+    }
+  },
+  mounted() {
+    db.collection('users')
+      .doc('PSNArFzFw7wuaMGOu6e3')
+      .get()
+      .then(snapshot => {
+        this.user = snapshot.data().name
+      })
+  },
   components: {
     Navigation
   }
 }
 </script>
-
 
 <style lang="scss">
 $primary: #dddd;
